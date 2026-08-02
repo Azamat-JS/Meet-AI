f751ac08df0495552c45ed5692c5cd382ff57c63
Azamat-JS <azamatielts987@gmail.com>
2026-08-02 21:23:54 +0500

    updated UI of sign in view

--- modules/auth/ui/views/sign-in-view.tsx
+++ modules/auth/ui/views/sign-in-view.tsx
@@ -1,10 +1,22 @@
 "use client"
 
-import { Card } from '@/components/ui/card'
+import { Card, CardContent } from '@/components/ui/card'
 
 const SignInView = () => {
     return (
-        <Card>SignInView</Card>
+        <div className="flex flex-col gap-6">
+
+            <Card className="overflow-hidden p-0">
+                <CardContent className="grid md:grid-cols-2 p-0">
+
+                    <form>col1</form>
+                    <div className="bg-radial from-green-700 to-green-900 relative hidden md:flex flex-col items-center justify-center gap-y-4">
+                        <p className="text-2xl font-semibold text-white">Meet.AI</p>
+                    </div>
+
+                </CardContent>
+            </Card>
+        </div>
     )
 }
 
